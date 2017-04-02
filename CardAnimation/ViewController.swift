8827bfce9742ca569740cab625a99cee974ddeeb
Brian Advent <brianadvent@Brians-MacBook-Pro.local>
2017-04-02 16:53:36 +0200

    Lottie Tutorial with dynamic content

--- CardAnimation/ViewController.swift
+++ CardAnimation/ViewController.swift
@@ -7,14 +7,69 @@
 //
 
 import UIKit
+import Lottie
+
 
 class ViewController: UIViewController {
 
+    var animationView:LOTAnimationView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        addAnimationView(withImageView: nil)
+        
+    }
+    
+    
+    func addAnimationView (withImageView imageView:UIImageView?) {
+    
+        if animationView != nil {
+            animationView?.removeFromSuperview()
+            animationView = nil
+        }
+        
+        
+        animationView = LOTAnimationView(name: "CardAnimation")
+        animationView?.center = self.view.center
+        animationView?.frame.origin.y -= 100
+        
+        if let newImage = imageView {
+            animationView?.addSubview(newImage, toLayerNamed: "Placeholder")
+        }
+        
+        
+        self.view.addSubview(animationView!)
+
+        
+        
+    
     }
 
+    
+    @IBAction func chooseImage(_ sender: UIButton) {
+        let imageView = UIImageView()
+        
+        if sender.tag == 0 {
+            imageView.image = UIImage(named: "img1")
+        }else{
+            imageView.image = UIImage(named: "img2")
+        }
+        
+        addAnimationView(withImageView: imageView)
+        
+        
+    }
+    
+    
+    @IBAction func playAnimation(_ sender: Any) {
+        if animationView != nil {
+            animationView?.play()
+        }
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
